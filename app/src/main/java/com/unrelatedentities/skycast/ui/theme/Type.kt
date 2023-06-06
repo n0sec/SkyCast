c1cdc81e9cee49cb1cce3e1e26795666de4c88fe
Nick <nconkl1@outlook.com>
2023-06-06 11:12:50 -0400

    updated to Montserrat Font

--- app/src/main/java/com/unrelatedentities/skycast/ui/theme/Type.kt
+++ app/src/main/java/com/unrelatedentities/skycast/ui/theme/Type.kt
@@ -15,110 +15,110 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs,
 )
 
-val BitterFont = GoogleFont(
-    "Bitter"
+val MontserratFont = GoogleFont(
+    "Montserrat",
 )
 
-val BitterFontFamily = FontFamily(
-    Font(googleFont = BitterFont, fontProvider = provider),
+val MontserratFontFamily = FontFamily(
+    Font(googleFont = MontserratFont, fontProvider = provider),
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         lineHeight = 52.sp,
         fontSize = 45.sp,
         letterSpacing = 0.sp,
     ),
     displaySmall = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         lineHeight = 44.sp,
         fontSize = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineLarge = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
     ),
     headlineMedium = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineSmall = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp,
     ),
     titleLarge = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = BitterFontFamily,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
